5019ef6a4591648eb3622ab37f949c5c7a41a9f2
Ryan Cornett <ryandcornett@gmail.com>
2024-01-11 14:22:41 -0500

    End of section 4

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities;
+
+public class Edit
+{
+    public class Command : IRequest
+    {
+        public Activity Activity { get; set; } // what we send from the client side
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public Handler(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IMapper Mapper { get; }
+
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            var activity = await _context.Activities.FindAsync(request.Activity.Id);
+            _mapper.Map(request.Activity, activity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
+public class Delete
+{
+    public class Command : IRequest
+    {
+        public Guid Id { get; set; } // what we send from the client side
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+        private readonly DataContext _context;
+
+        public Handler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IMapper Mapper { get; }
+
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            var activity = await _context.Activities.FindAsync(request.Id);
+            _context.Remove(activity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Application/Activities/List.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Domain;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using System.Linq.Expressions;
+
+namespace Application.Activities;
+
+public class List
+{
+    public class Query : IRequest<List<Activity>> {}
+
+    public class Handler : IRequestHandler<Query, List<Activity>>
+    {
+        private readonly DataContext _context;
+
+        public Handler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ILogger<List> Logger { get; }
+
+        public async Task<List<Activity>> Handle(Query request, CancellationToken cancellation)
+        {
+            return await _context.Activities.ToListAsync();
+        }
+    }
+}
